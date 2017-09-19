ecee049262f7237afef883d0fad2ca956dfa3b8f
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2017-09-19 16:18:51 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,44 @@
-from setuptools import setup, Extension
+from setuptools import setup, Extension, Command
 import setuptools.command.build_ext
+import setuptools.command.install
+import distutils.command.build
+import subprocess
 import sys
 import os
 
-class numpy_build_ext(build_ext):
+def run_ale_install_script():
+    # If we need to give compiler options this is where we could pass to script
+    build_libs_cmd = ['bash', 'ale_install.sh']
+    if subprocess.call(build_libs_cmd) != 0:
+        sys.exit("Failed to build ALE dependencies")
+
+class build_ale(Command):
+    def run(self):
+        run_ale_install_script()
+
+class install(setuptools.command.install.install):
+    def run(self):
+        self.run_command('build_ale')
+        setuptools.command.install.install.run(self)
+
+class build(distutils.command.build.build):
+    sub_commands = [
+        ('build_ale', lambda self: True),
+        ] + distutils.command.build.build.sub_commands
+
+class build_ext(setuptools.command.build_ext.build_ext):
     """build_ext command for use when numpy headers are needed."""
     def run(self):
         try:
             import numpy as np
-            self.include_dirs.append(np.get_include() + '/numpy')
+            numpy_include_path = np.get_include() + '/numpy'
+            if numpy_include_path not in self.include_dirs:
+                self.include_dirs.append(numpy_include_path)
         except ImportError:
             sys.exit("Error: Numpy required")
 
         # Call original build_ext command
-        build_ext.run(self)
+        setuptools.command.build_ext.build_ext.run(self)
 
 # Remove setuptools dumb c warnings
 import distutils.sysconfig
@@ -87,7 +112,8 @@ ale_module = Extension('ale_generator',
                     extra_compile_args=extra_compile_args,
                     include_dirs=include_dirs,
                     library_dirs=library_dirs,
-                    extra_link_args=extra_link_args + main_link_args) #['-Wl,-rpath,$ORIGIN/??/lib']
+                    extra_link_args=extra_link_args + main_link_args
+                        + ['-Wl,-rpath,$ORIGIN/../lib'])
 
 agent_module = Extension('agent_generator',
                     language = "c++14",
@@ -96,7 +122,8 @@ agent_module = Extension('agent_generator',
                     extra_compile_args=extra_compile_args,
                     include_dirs=include_dirs,
                     library_dirs=library_dirs,
-                    extra_link_args=extra_link_args + main_link_args)
+                    extra_link_args=extra_link_args + main_link_args
+                        + ['-Wl,-rpath,$ORIGIN/../lib'])
 
 objective_module = Extension('total_cost_objective',
                     language = "c++14",
@@ -105,12 +132,14 @@ objective_module = Extension('total_cost_objective',
                     extra_compile_args=extra_compile_args,
                     include_dirs=include_dirs,
                     library_dirs=library_dirs,
-                    extra_link_args=extra_link_args + main_link_args)
+                    extra_link_args=extra_link_args + main_link_args
+                        + ['-Wl,-rpath,$ORIGIN/../lib'])
 
 setup(name=PACKAGE_NAME,
       version='1.0',
       author='Nathaniel Rodriguez',
-      cmdclass = {'build_ext': numpy_build_ext},
+      cmdclass = {'build_ext': build_ext, 'install': install,
+                  'build_ale': build_ale, 'build_py':build_py},
       description='A wrapper for a ctrnn implementation of ALE',
       url='https://github.com/neuro-evolution/alectrnn.git',
       install_requires=[
