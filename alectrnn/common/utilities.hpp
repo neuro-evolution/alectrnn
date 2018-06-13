65f9f76b11846f4f111812853e28d20cdd05cd42
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2018-06-13 10:05:41 -0400

    added comment

--- alectrnn/common/utilities.hpp
+++ alectrnn/common/utilities.hpp
@@ -76,6 +76,7 @@ inline bool is_a_ge_zero_and_a_lt_b(Integer a, Integer b) {
  * Modified from caffe's implementation @
  * https://github.com/BVLC/caffe/blob/master/src/caffe/util/im2col.cpp
  * Uses NCHW memory layout
+ * Applies zero padding
  */
 template <typename Dtype>
 void Im2Col(const Dtype* data_im, const Integer channels,
