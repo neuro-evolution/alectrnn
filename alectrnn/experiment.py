642682761bbf803362f5ddf2108020c9209d185d
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2018-07-09 14:32:54 -0400

    added base class for experiment

--- alectrnn/experiment.py
+++ alectrnn/experiment.py
@@ -1,13 +1,11 @@
 from alectrnn import handlers
 from alectrnn import nervous_system as ns
+from abc import ABC, abstractmethod
 
 
-class ALEExperiment:
+class ALEExperimentBase(ABC):
     """
-    This class is for creating and managing an ALE experiment. The user gives
-    parameters for the game, objective, and neural network. The class setups
-    the necessary handlers and the user can then access the objective function
-    for use in optimization
+    Defines functions that all experiment classes should have
     """
 
     def __init__(self, ale_parameters, nervous_system_class,
@@ -48,41 +46,187 @@ class ALEExperiment:
         """
 
         # Save parameters
+        self.nervous_system_class = nervous_system_class
+        self.agent_class = agent_class
         self.ale_parameters = ale_parameters
         self.nervous_system_class_parameters = nervous_system_class_parameters
         self.agent_class_parameters = agent_class_parameters
         self.objective_parameters = objective_parameters
+        self.script_prefix = script_prefix
 
-        # Construct handles (ALE)
-        self._ale_handle = handlers.ALEHandler(**ale_parameters)
-        self._ale_handle.create()
+    def construct_nervous_system(self, nervous_system_class,
+                                 nervous_system_parameters, agent_class,
+                                 ale_handle):
+        """
+        :param nervous_system_class: class used to generate the nervous system
+        :param nervous_system_parameters: parameters for the class
+        :param agent_class: class used for generating the agent
+        :param ale_handle: a built ale handler object
+        :return: a NervousSystem object
+        """
 
-        # Construct handle (Nervous system)
         if isinstance(agent_class, handlers.SharedMotorAgentHandler):
             # SharedMotorAgents require the legal rather than minimal action
             # set size to be used for the motor layer
-            self.nervous_system_class_parameters['num_outputs'] = \
-                self._ale_handle.legal_action_set_size()
+            nervous_system_parameters['num_outputs'] = \
+                ale_handle.legal_action_set_size()
         else:
-            self.nervous_system_class_parameters['num_outputs'] = \
-                self._ale_handle.action_set_size()
+            nervous_system_parameters['num_outputs'] = \
+                ale_handle.action_set_size()
 
-        self._nervous_system = nervous_system_class(**self.nervous_system_class_parameters)
+        return nervous_system_class(**nervous_system_parameters)
+
+    def construct_ale_handle(self, ale_parameters):
+        """
+        :param ale_parameters: dictionary of ale parameters
+        :return: an operational handle
+        """
+        ale_handle = handlers.ALEHandler(**ale_parameters)
+        ale_handle.create()
+        return ale_handle
+
+    def construct_agent_handle(self, agent_class, partial_class_parameters,
+                               nervous_system, ale_handle):
+        """
+        Builds and initializes the agent handle and creates the c++-object
+        :param agent_class: class used for making the agent
+        :param partial_class_parameters: dictionary that includes everything
+            but the nervous system, since that will be built locally
+        :param nervous_system: the built nervous system
+        :param ale_handle: an ale_handle
+        :return: an agent handler object
+        """
+        partial_class_parameters['nervous_system'] = nervous_system.neural_network
+        agent_handle = agent_class(ale_handle.handle, **partial_class_parameters)
+        agent_handle.create()
+        return agent_handle
+
+    def construct_objective_handle(self, objective_parameters, ale_handle,
+                                   agent_handle):
+        """
+        :param objective_parameters: dictionary of parameters for the objective
+        :param ale_handle: a built ale handler object
+        :param agent_handle: a built agent handler object
+        :return: an objective handler object
+        """
+        obj_handle = handlers.ObjectiveHandler(ale_handle.handle,
+                                               agent_handle.handle,
+                                               **objective_parameters)
+        obj_handle.create()
+        return obj_handle
+
+    def parameter_layout(self):
+        """
+        :return: The parameters lay out as a numpy array valued by
+            handlers.PARAMETER_TYPE values
+        """
+        return self._nervous_system.parameter_layout()
+
+    def get_parameter_count(self):
+        """
+        :return: The number of parameters in the neural network
+        """
+        return self._nervous_system.get_parameter_count()
+
+    def screen_history(self):
+        """
+        :return: numpy array of the screen state for each time-step
+        """
+        return self._agent_handle.screen_history()
+
+    def layer_history(self, layer_index):
+        """
+        :param layer_index: The index of the layer you want to get the history of
+        :return: a numpy array of the state for each time-step
+        """
+        return self._agent_handle.layer_history(layer_index)
+
+    def print_layer_shapes(self):
+        """
+        Prints the shapes calculated for each layer
+        :note: not to be confused with interpreted shapes
+        """
+
+        for i, shape in enumerate(self._nervous_system.layer_shapes):
+            print("Layer", i, " with shape", shape)
+
+    def num_layers(self):
+        """
+        :return: The number of layers in the neural network
+        """
+        return self._nervous_system.num_layers()
+
+    def draw_initial_guess(self, type_bounds, rng, normalized_weights=True,
+                           norm_type='sqrt'):
+        """
+        :param type_bounds: a dictionary with low/high bounds for each type.
+            If weights are set too 'norm', then they are initialized with a
+            normalization procedure.
+        :param rng: a seeded numpy RandomState
+        :param normalized_weights: True (ignores WEIGHTS bounds and chooses
+            weights between [-1/N, 1/N], where N==# of pre-synaptic connections)
+        :param norm_type: 'sqrt' for 1/sqrt(N) (default)
+                          'norm' for 1/N
+        :return: a 1D numpy float 32 array
+        """
+
+        return ns.draw_initial_guess(type_bounds,
+                                     self._nervous_system,
+                                     rng, normalized_weights, norm_type)
+
+    @abstractmethod
+    def set_logging(self, is_logging):
+        """
+        Changes the logging state of the experiment. Requires remaking the agent
+        and objective handles.
+        :param is_logging: True/False
+        :return: None
+        """
+
+        self._agent_handle.logging = is_logging
+        self._obj_handle.agent = self._agent_handle.handle
+
+    @property
+    @abstractmethod
+    def objective_function(self):
+        """
+        :return: returns the objective function
+        """
+        pass
+
+
+class ALEExperiment(ALEExperimentBase):
+    """
+    This class is for creating and managing an ALE experiment. The user gives
+    parameters for the game, objective, and neural network. The class setups
+    the necessary handlers and the user can then access the objective function
+    for use in optimization
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        # Construct handles (ALE)
+        self._ale_handle = self.construct_ale_handle(self.ale_parameters)
+
+        # Construct handle (Nervous system)
+        self._nervous_system = self.construct_nervous_system(self.nervous_system_class,
+                                                             self.nervous_system_class_parameters,
+                                                             self.agent_class,
+                                                             self._ale_handle)
 
         # Construct handle (Agent)
-        self.agent_class = agent_class
-        self.agent_class_parameters['nervous_system'] = self._nervous_system.neural_network
-        self._agent_handle = self.agent_class(self._ale_handle.handle,
-                                              **self.agent_class_parameters)
-        self._agent_handle.create()
+        self._agent_handle = self.construct_agent_handle(self.agent_class,
+                                                         self.agent_class_parameters,
+                                                         self._nervous_system.neural_network,
+                                                         self._ale_handle)
 
         # Construct handle (objective)
-        self._obj_handle = handlers.ObjectiveHandler(self._ale_handle.handle,
-                                                     self._agent_handle.handle,
-                                                     **self.objective_parameters)
-        self._obj_handle.create()
+        self._obj_handle = self.construct_objective_handle(self.objective_parameters,
+                                                           self._ale_handle,
+                                                           self._agent_handle)
 
-        self.script_prefix = script_prefix + "_" \
+        self.script_prefix = self.script_prefix + "_" \
                              + self._ale_handle.rom + "_npar" \
                              + str(self.get_parameter_count())
 
@@ -114,19 +258,6 @@ class ALEExperiment:
                                                      **objective_parameters)
         self._obj_handle.create()
 
-    def get_parameter_count(self):
-        """
-        :return: The number of parameters in the neural network
-        """
-        return self._nervous_system.get_parameter_count()
-
-    def parameter_layout(self):
-        """
-        :return: The parameters lay out as a numpy array valued by
-            handlers.PARAMETER_TYPE values
-        """
-        return self._nervous_system.parameter_layout()
-
     def set_game_parameters(self, **kwargs):
         """
         :param kwargs: any keyword arguments used by the ale environment
@@ -153,10 +284,8 @@ class ALEExperiment:
         :param is_logging: True/False
         :return: None
         """
-
-        self._agent_handle.logging = is_logging
-        self._obj_handle.agent = self._agent_handle.handle
-
+        super().set_logging(is_logging)
+        
     def _update_game_environment(self):
         """
         updates the necessary handles when the game environment is updated
@@ -165,55 +294,9 @@ class ALEExperiment:
         self._obj_handle.agent = self._agent_handle.handle
         self._obj_handle.ale = self._ale_handle.handle
 
-    def screen_history(self):
-        """
-        :return: numpy array of the screen state for each time-step
-        """
-        return self._agent_handle.screen_history()
-
-    def layer_history(self, layer_index):
-        """
-        :param layer_index: The index of the layer you want to get the history of
-        :return: a numpy array of the state for each time-step
-        """
-        return self._agent_handle.layer_history(layer_index)
-
-    def print_layer_shapes(self):
-        """
-        Prints the shapes calculated for each layer
-        :note: not to be confused with interpreted shapes
-        """
-
-        for i, shape in enumerate(self._nervous_system.layer_shapes):
-            print("Layer", i, " with shape", shape)
-
-    def num_layers(self):
-        """
-        :return: The number of layers in the neural network
-        """
-        return self._nervous_system.num_layers()
-
     @property
     def objective_function(self):
         return self._obj_handle.handle
-
-    def draw_initial_guess(self, type_bounds, rng, normalized_weights=True,
-                           norm_type='sqrt'):
-        """
-        :param type_bounds: a dictionary with low/high bounds for each type.
-            If weights are set too 'norm', then they are initialized with a
-            normalization procedure.
-        :param rng: a seeded numpy RandomState
-        :param normalized_weights: True (ignores WEIGHTS bounds and chooses
-            weights between [-1/N, 1/N], where N==# of pre-synaptic connections)
-        :param norm_type: 'sqrt' for 1/sqrt(N) (default)
-                          'norm' for 1/N
-        :return: a 1D numpy float 32 array
-        """
-
-        return ns.draw_initial_guess(type_bounds,
-                                     self._nervous_system,
-                                     rng, normalized_weights, norm_type)
 
 
 if __name__ == '__main__':
